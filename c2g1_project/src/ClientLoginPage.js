10ff5e9425c3076c215003ab072afb0c4b24c6d5
Jae <gohyishen777@gmail.com>
2024-06-24 20:47:12 +0800

    feature: Updated designs of all login pages

--- c2g1_project/src/ClientLoginPage.js
+++ c2g1_project/src/ClientLoginPage.js
@@ -1,6 +1,10 @@
 import TrainerLoginPage from './TrainerLoginPage';
 import ClientHomePage from './ClientHomePage'; 
 import { useNavigate} from 'react-router-dom';
+import stockimgtop from './stockimgtop.jpg';
+import stockimgbottom from './stockimgbottom.jpg';
+import dellacademylogo from './DellAcademy.png';
+
 
 const ClientLoginPage = () => {
    const nav = useNavigate();
@@ -8,28 +12,42 @@ const ClientLoginPage = () => {
       nav("/ClientHomePage");
    }
    const handleTrainerLoginPage = () => {
+      alert("Navigating to Trainer Log In")
       nav("/TrainerLoginPage");
    }
    const handleAdminLoginPage = () => {
+      alert("Navigating to Admin Log In")
       nav("/AdminLoginPage");
    }
 
       return ( 
-        <>            
-           <h1 className="login_words">Grow your skills with Dell Academy</h1>
-           <div className="login_page">
-              <div className="login_pictures">
-                 <p>Client sign in</p>
-              </div>
-              <div className="login_buttons">
-                  <button className="client_login_button_blue">Client</button>
-                  <button className="admin_login_button" onClick={handleAdminLoginPage}>Admin</button>
-                  <button className="trainer_login_button" onClick={handleTrainerLoginPage}>Trainer</button>
-                  <button className="signin" onClick={handleSignIn}>Sign in</button>
-              </div>
-           </div>
+        <>
+                
+         <div className='top_of_login'>
+         <div className='login_words'>
+            <h1>Grow your skills with Dell Academy</h1>
+            <h4>Sign up for Award Winning Workshops Today!</h4>
+         </div>
+         <div>
+            <img src={dellacademylogo} alt="logo"></img>
+         </div>
+         </div>           
+         <div className="login_page">   
+            <div className="login_pictures">
+               <img src={stockimgtop} alt="Stock Image"/>
+               <img src={stockimgbottom} alt="Stock Image"/>
+            </div>
+            <div className="login_buttons">
+               <button className="client_login_button_blue">Client</button>
+               <button className="admin_login_button" onClick={handleAdminLoginPage}>Admin</button>
+               <button className="trainer_login_button" onClick={handleTrainerLoginPage}>Trainer</button>
+            </div>
+            <div className='login_details'>
+             <button className="signin_button" onClick={handleSignIn}>Sign in</button>
+            </div>
+         </div>
         </>
-       );
+       )
   }
  
 export default ClientLoginPage;
