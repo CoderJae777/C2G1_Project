2caf96c4392b292707e2bdc5b954de17198223ea
mhrkq <amptytenk@gmail.com>
2024-06-25 12:06:40 +0800

    feature: added images folder, started AdminHomePage

--- c2g1_project/src/AdminHomePage.js
+++ c2g1_project/src/AdminHomePage.js
@@ -1,11 +1,22 @@
+import './homepage.css';
+import dellacademylogo from './images/DellAcademy.png';
+import userprofilepic from './images/userprofilepic.png';
+
 const AdminHomePage = () => {
-    
     return ( 
         <div>
-        <h1>AdminHomePage</h1>
-        <h2>hi</h2>
+            <div className="delllogo"></div>
+            <div>
+                <img src={dellacademylogo} alt="Dell Academy Logo" />
+            </div>
+            <div class="leftsidebar">
+                <div class="userprofilepic">
+                    <img class="userprofilepic" src={userprofilepic} alt="User Profile Pic" />
+                </div>    
+                <h1 class="username">Dil Doe</h1>
+            </div>
         </div>
-     );
+    );
 }
  
 export default AdminHomePage;
