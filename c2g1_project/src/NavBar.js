f2134d7e3cb0fa9ec583f9a04fd29f4fe4e75d5c
Jae <gohyishen777@gmail.com>
2024-06-26 17:56:15 +0800

    feature: Updated design of login page

--- c2g1_project/src/NavBar.js
+++ c2g1_project/src/NavBar.js
@@ -1,5 +1,8 @@
 import { useNavigate} from 'react-router-dom';
 import LoginPage from './LoginPage';
+import SignUpPage from './SignUpPage';
+import dellacademylogo_small from './images/NavBarLogo.png';
+
 
 
 const Navbar = () => {
@@ -9,17 +12,23 @@ const Navbar = () => {
     const handleNavBarSignIn = () => {
        nav("/");
     }
+    const handleSignUp = () => {
+        nav("/SignUpPage");
+     }
+  
 
     return ( 
         <nav className="navbar">
             <div className="navbar_contents">
+                <img onClick={handleNavBarSignIn} className='navbarlogo' src={dellacademylogo_small}></img>
                 {/* TBC --> links to an about us page */}
                 <button>Dell Academy</button>
                 {/* TBC --> links to the workshops page */}
                 <button>Our Workshops</button>
                 {/* TBC --> links to a view all trainers page */}
                 <button>Our Trainers</button>
-                <button onClick={handleNavBarSignIn}>Log in/out!</button>
+                <button onClick={handleNavBarSignIn}>Log in</button>
+                <button onClick={handleSignUp}>Sign Up!</button>
             </div>
         </nav>
      );
