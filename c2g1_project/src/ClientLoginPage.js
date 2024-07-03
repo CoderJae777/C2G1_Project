85d422a0053f9969c492037f43d6120a1c73b0f4
Jae <gohyishen777@gmail.com>
2024-07-03 18:13:29 +0800

    feature: Animation, Graphs, Removed navbars

--- c2g1_project/src/ClientLoginPage.js
+++ c2g1_project/src/ClientLoginPage.js
@@ -9,10 +9,12 @@ import { useState } from 'react';
 import AdminHomePage from './AdminHomePage';
 import { useEffect } from "react";
 import ClientHomePage from './ClientHomePage';
+import { motion } from "framer-motion";
 
 import useAxiosPost from "./api/useAxiosPost.jsx";
 import { config } from "./config/config.js";
 import { endpoints } from "./config/endpoints.js";
+import Navbar from './NavBar.js';
 
 // Running Json Server
 // npx json-server --watch db.json --port 8000
@@ -23,6 +25,7 @@ const ClientLoginPage = () => {
    const nav = useNavigate();
    const [username, usernameupdate] = useState("");
    const [password, passwordupdate] = useState("");
+   const [move, setMove] = useState(false);
 
    const handleSuccess = (data) => {
       nav('/AdminHomePage');
@@ -79,18 +82,13 @@ const ClientLoginPage = () => {
 
    return (
       <>
-         <div className='top_of_login'>
-
-            <div>
-
-            </div>
-         </div>
+      <Navbar/>
          <div className="login_page">
-            <div className="login_pictures">
-               <img src={stockimgtop} alt="Stock Image" />
-            </div>
-            <div className="login_buttons">
-               <img src={dellacademylogo} className="dell_logo" alt="logo"></img>
+         <motion.div animate={{x : move ? 0:200}} transition={{ type: "inertia", velocity: 40 }} className="login_pictures">
+         <img src={stockimgtop} alt="Stock Image" />
+            </motion.div>
+            <motion.div animate={{scale:1}} initial={{scale:0}} transition={{type:"spring" , delay:0.25}} className="login_buttons">
+            <img src={dellacademylogo} className="dell_logo" alt="logo"></img>
                <h5>I am a/ an ... </h5>
                <button className="client_login_button_blue">Client</button>
                <button className="admin_login_button" onClick={handleAdminLoginPage}>Admin</button>
@@ -118,7 +116,7 @@ const ClientLoginPage = () => {
                      <h5 className='forget_pw'>Forget password</h5>
                   </div>
                </form>
-            </div>
+            </motion.div>
 
          </div>
       </>
