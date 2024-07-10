93e30f6aca93dff65f0cfb6314e36c7244757015
Clarencelaujw <clarencelaujw@gmail.com>
2024-07-10 17:12:52 +0800

    /feat: sync adminhomepage graphs to backend

--- c2g1_project/src/components/useFetch.js
+++ c2g1_project/src/components/useFetch.js
@@ -6,7 +6,10 @@ const useFetch = () => {
   const [today_data, setTodayData] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:8000/trainer_data/")
+    fetch("http://localhost:5001/products/trainerGraph", {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    })
       .then((res) => res.json())
       .then((data) => {
         setTrainerData(data);
@@ -14,18 +17,33 @@ const useFetch = () => {
   }, []); // Empty dependency array ensures useEffect runs only once
 
   useEffect(() => {
-    fetch("http://localhost:8002/workshop_data/")
-      .then((res) => res.json())
-      .then((data2) => {
-        setWorkshopData(data2);
-      });
+    fetch("http://localhost:5001/products/workshopGraph", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data.data);
+        setWorkshopData(data.data);
+      })
+      .catch((error) => console.error("Error:", error));
+
+    // .then((data2) => {
+    //   console.log("here", data2);
+    //   setWorkshopData(data2);
+    // });
   }, []); // Empty dependency array ensures useEffect runs only once
 
   useEffect(() => {
-    fetch("http://localhost:8003/today_data/")
+    fetch("http://localhost:5001/products/todayGraph", {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    })
       .then((res) => res.json())
       .then((data3) => {
-        setTodayData(data3);
+        setTodayData(data3.data);
       });
   }, []); // Empty dependency array ensures useEffect runs only once
 
